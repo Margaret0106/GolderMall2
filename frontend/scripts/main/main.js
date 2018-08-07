720233db947a54a7d78ed5845b00fe88faea4d96
Margarita <margarita@rocketfirm.com>
2018-08-08 00:21:36 +0600

    new pages

--- frontend/scripts/main/main.js
+++ frontend/scripts/main/main.js
@@ -127,4 +127,14 @@ $(document).ready(function () {
     }, 500)
   })
 
+
+  $('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
+
+
 });
